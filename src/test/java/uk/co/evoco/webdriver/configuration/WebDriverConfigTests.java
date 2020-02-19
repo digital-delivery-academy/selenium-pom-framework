e3fcf1c42b13be1a11c33d580b83b109e487ea15
Augustine Kwanashie <augustine.kwanashie@engineering.digital.dwp.gov.uk>
2020-02-19 12:31:37 +0000

    Allow the WebDriverConfig class handle browserPreferencess
    We want to be able to include browserPreferences as a key in the root of the config file
    and have the resulting WebDriverConfig object expose methods to fetch specific properties
    based on the provided browser type. This can then be picked up when the drivers are created.
    
    This commit only lets the WebDriverCongfig class support this extra field.
    Following commits will have the code that includes the configs for the supported driver types.
    
    In terms of testing, we have not included fixture files for the various test cases (as is the case in the other tests)
    So we should probably have a chat about that and refactor the code to either use the fixture files or leave as-is.
    
    The reason  we choose not to crerate the extra fixture files is that it would require at least 3 extra files for the 4 test cases
    wich can be a bit excessive especially as defining the input inline does not make the code less readable?
    Will be happy to change this approach if others think otherwise.
    
    We also handled the scenario where the case of the browser type does not matter.
    This can also be removed if deemed unnecessary.

--- src/test/java/uk/co/evoco/webdriver/configuration/WebDriverConfigTests.java
+++ src/test/java/uk/co/evoco/webdriver/configuration/WebDriverConfigTests.java
@@ -1,6 +1,10 @@
 package uk.co.evoco.webdriver.configuration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 import uk.co.evoco.webdriver.utils.JsonUtils;
 
@@ -23,6 +27,52 @@ public class WebDriverConfigTests {
         assertThat(webDriverConfig.getTestConfigItem("sample"), is("sample text"));
         assertThat(webDriverConfig.getRunType(), is(RunType.LOCAL));
         assertThat(webDriverConfig.getGridConfig().getGridUrl().toString(), is("http://localhost:4444/wd/hub"));
+    }
+
+    @Test
+    public void testGetBrowserPreferencesReturnsAnEmptyNodeIfNoBrowserOptionsArePresentInConfigJson() throws JsonProcessingException {
+        String configJson = "{}";
+        WebDriverConfig webDriverConfig = JsonUtils.fromString(configJson, WebDriverConfig.class);
+
+        JsonNode actualPreferences = webDriverConfig.getBrowserPreferences(BrowserType.CHROME);
+        assertThat(actualPreferences, is(JsonNodeFactory.instance.objectNode()));
+    }
+
+    @Test
+    public void testGetBrowserPreferencesReturnsAnEmptyNodeIfSpecifiedBrowserTypeIsNotPresentInConfigJson() throws JsonProcessingException {
+        String configJson = "{ \"browserPreferences\": {}}";
+        WebDriverConfig webDriverConfig = JsonUtils.fromString(configJson, WebDriverConfig.class);
+
+        JsonNode actualPreferences = webDriverConfig.getBrowserPreferences(BrowserType.CHROME);
+        assertThat(actualPreferences, is(JsonNodeFactory.instance.objectNode()));
+    }
+
+    @Test
+    public void testGetBrowserPreferencesReturnsTheCorrectBrowserOptions() throws JsonProcessingException {
+        String preferenceKey = "browser.download.dir";
+        String preferenceValue = "docs/chrome/";
+        String inputConfigJson = String.format("{ \"browserPreferences\": { \"chrome\": {\"%s\": \"%s\"}}}", preferenceKey, preferenceValue);
+
+        WebDriverConfig webDriverConfig = JsonUtils.fromString(inputConfigJson, WebDriverConfig.class);
+        JsonNode actualPreferences = webDriverConfig.getBrowserPreferences(BrowserType.CHROME);
+
+        ObjectNode expectedPreferences = JsonNodeFactory.instance.objectNode()
+                .put(preferenceKey, preferenceValue);
+        assertThat(actualPreferences, is(expectedPreferences));
+    }
+
+    @Test
+    public void testGetBrowserPropertiesReturnsTheCorrectBrowserOptionsIrrespectiveOfCase() throws JsonProcessingException {
+        String preferenceKey = "browser.download.dir";
+        String preferenceValue = "docs/chrome/";
+        String inputConfigJson = String.format("{ \"browserPreferences\": { \"CHROME\": {\"%s\": \"%s\"}}}", preferenceKey, preferenceValue);
+
+        WebDriverConfig webDriverConfig = JsonUtils.fromString(inputConfigJson, WebDriverConfig.class);
+        JsonNode actualPreferences = webDriverConfig.getBrowserPreferences(BrowserType.CHROME);
+
+        ObjectNode expectedPreferences = JsonNodeFactory.instance.objectNode()
+                .put(preferenceKey, preferenceValue);
+        assertThat(actualPreferences, is(expectedPreferences));
     }
 
     @Test
