cc3b5d5c5e4376ac2b3cf2d39c9d191e233510ab
Augustine Kwanashie <augustine.kwanashie@engineering.digital.dwp.gov.uk>
2020-02-19 16:45:04 +0000

    Include firefox preferences from config into driver
    We want to be able to load user-specified preferences from config (if it exists) onto the driver.
    This has to be browser specific as the techniques for each of the browsers are different.
    
    This commit adds the necessary code for including firefox preferences when creating a firefox driver.

--- src/test/java/uk/co/evoco/webdriver/configuration/driver/ConfiguredFirefoxDriverIT.java
+++ src/test/java/uk/co/evoco/webdriver/configuration/driver/ConfiguredFirefoxDriverIT.java
@@ -3,11 +3,14 @@ package uk.co.evoco.webdriver.configuration.driver;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ConfiguredFirefoxDriverIT {
@@ -17,5 +20,19 @@ public class ConfiguredFirefoxDriverIT {
         ConfiguredFirefoxDriver configuredFirefoxDriver = new ConfiguredFirefoxDriver();
         WebDriver webDriver = configuredFirefoxDriver.getDriver(FileUtils.getTempDirectory());
         assertThat(webDriver, instanceOf(EventFiringWebDriver.class));
+    }
+
+    @Test
+    public void testGetOptionsReturnsOptionsIncludedInFireFoxConfig() {
+        ConfiguredFirefoxDriver configuredFirefoxDriver = new ConfiguredFirefoxDriver();
+        Map<String, Object> firefoxPreferences = getPreferences(configuredFirefoxDriver.getOptions());
+        assertThat(firefoxPreferences.get("pdfjs.disabled"), is(true));
+        assertThat(firefoxPreferences.get("browser.download.folderList"), is(2));
+        assertThat(firefoxPreferences.get("browser.download.defaultFolder"), is("downloads/reports"));
+    }
+
+    private Map<String, Object> getPreferences (FirefoxOptions options) {
+        Map<String, Object> moxOptions = (Map<String, Object>)options.asMap().get("moz:firefoxOptions");
+        return (Map<String, Object>)moxOptions.get("prefs");
     }
 }
