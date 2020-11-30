35c0a6ef9707e06e913e1a67513ea77116a3fc3d
Steve Walton <steve.walton@evoco.co.uk>
2020-11-30 23:43:37 +0000

    Attempting fix to saucelabs in CI

--- src/test/java/uk/co/evoco/webdriver/configuration/driver/ConfiguredSauceLabsDriverTests.java
+++ src/test/java/uk/co/evoco/webdriver/configuration/driver/ConfiguredSauceLabsDriverTests.java
@@ -1,11 +1,8 @@
 package uk.co.evoco.webdriver.configuration.driver;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import uk.co.evoco.exceptions.SauceLabsCredentialsException;
-
-import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -13,10 +10,10 @@ import static org.hamcrest.Matchers.containsString;
 public class ConfiguredSauceLabsDriverTests {
 
     @Test
-    public void testCanRunTestOnSauceLabsUsingConfigiration() throws SauceLabsCredentialsException, IOException {
+    public void testCanRunTestOnSauceLabsUsingConfigiration() throws SauceLabsCredentialsException{
         System.setProperty("config", "config-saucelabs.json");
         ConfiguredDriver configuredSauceLabsGridDriver = new ConfiguredSauceLabsGridDriver();
-        WebDriver webDriver = configuredSauceLabsGridDriver.getDriver(FileUtils.getTempDirectory());
+        WebDriver webDriver = configuredSauceLabsGridDriver.getRemoteDriver();
         webDriver.get("https://www.evoco.co.uk");
         assertThat("Didn't make it to evoco.co.uk", webDriver.getCurrentUrl(), containsString("evoco.co.uk"));
         webDriver.quit();
